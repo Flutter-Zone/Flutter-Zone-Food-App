1630c3e9d407b8b78534946fec816148de370bc5
Flutter Zone <letsflatter@gmail.com>
2020-03-31 21:46:12 +0000

    Completed the profile page

--- lib/src/pages/profile_page.dart
+++ lib/src/pages/profile_page.dart
@@ -7,142 +7,255 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
+  bool turnOnNotification = false;
+  bool turnOnLocation = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              "Profile",
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+              child: Container(
+          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "Profile",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  height: 120.0,
-                  width: 120.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(60.0),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 3.0,
-                          offset: Offset(0, 4.0),
-                          color: Colors.black38),
-                    ],
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/images/breakfast.jpeg",
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 120.0,
+                    width: 120.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(60.0),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 3.0,
+                            offset: Offset(0, 4.0),
+                            color: Colors.black38),
+                      ],
+                      image: DecorationImage(
+                        image: AssetImage(
+                          "assets/images/breakfast.jpeg",
+                        ),
+                        fit: BoxFit.cover,
                       ),
-                      fit: BoxFit.cover,
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Richmond Blankson",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      "+233247656959",
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      height: 25.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.blue,
-                          ),
-                          borderRadius: BorderRadius.circular(20.0)),
-                      child: Center(
-                        child: Text(
-                          "Edit",
-                          style: TextStyle(color: Colors.blue, fontSize: 16.0),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Richmond Blankson",
+                        style: TextStyle(
+                          fontSize: 16.0,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              "Account",
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        "+233247656959",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Container(
+                        height: 25.0,
+                        width: 60.0,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.blue,
+                            ),
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Center(
+                          child: Text(
+                            "Edit",
+                            style: TextStyle(color: Colors.blue, fontSize: 16.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: 20.0,),
-            Card(
-              elevation: 3.0,
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: <Widget>[
-                    CustomListTile(
-                      icon: Icons.location_on,
-                      text: "Location",
-                    ),
-                    Divider(
-                      height: 10.0,
-                      color: Colors.grey,
-                    ),
-                    CustomListTile(
-                      icon: Icons.visibility,
-                      text: "Change Password",
-                    ),
-                    Divider(
-                      height: 10.0,
-                      color: Colors.grey,
-                    ),
-                    CustomListTile(
-                      icon: Icons.shopping_cart,
-                      text: "Shipping",
-                    ),
-                    Divider(
-                      height: 10.0,
-                      color: Colors.grey,
-                    ),
-                    CustomListTile(
-                      icon: Icons.payment,
-                      text: "Payment",
-                    ),
-                    Divider(
-                      height: 10.0,
-                      color: Colors.grey,
-                    ),
-                  ],
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "Account",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 20.0,),
+              Card(
+                elevation: 3.0,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: <Widget>[
+                      CustomListTile(
+                        icon: Icons.location_on,
+                        text: "Location",
+                      ),
+                      Divider(
+                        height: 10.0,
+                        color: Colors.grey,
+                      ),
+                      CustomListTile(
+                        icon: Icons.visibility,
+                        text: "Change Password",
+                      ),
+                      Divider(
+                        height: 10.0,
+                        color: Colors.grey,
+                      ),
+                      CustomListTile(
+                        icon: Icons.shopping_cart,
+                        text: "Shipping",
+                      ),
+                      Divider(
+                        height: 10.0,
+                        color: Colors.grey,
+                      ),
+                      CustomListTile(
+                        icon: Icons.payment,
+                        text: "Payment",
+                      ),
+                      Divider(
+                        height: 10.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "Notifications",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              Card(
+                elevation: 3.0,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "App Notification",
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Switch(
+                            value: turnOnNotification,
+                            onChanged: (bool value){
+                              // print("The value: $value");
+                              setState(() {
+                                turnOnNotification = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        height: 10.0,
+                        color: Colors.grey,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Location Tracking",
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Switch(
+                            value: turnOnLocation,
+                            onChanged: (bool value){
+                              // print("The value: $value");
+                              setState(() {
+                                turnOnLocation = value;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        height: 10.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+                
+              ),
+                            SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                "Other",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              Card(
+
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Language", style: TextStyle(fontSize: 16.0)),
+                      // SizedBox(height: 10.0,),
+                      Divider(
+                        height: 30.0,
+                        color: Colors.grey,
+                      ),
+                      Text("Currency", style: TextStyle(fontSize: 16.0)),
+                      // SizedBox(height: 10.0,),
+                      Divider(
+                        height: 30.0,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
